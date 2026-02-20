8a7f28befaeb45e30f1cf144958804592fd9c7b4
Your Name <you@example.com>
2026-02-20 21:28:15 +0530

    Change Food category icon to crossed fork and knife

--- components/TransactionList.tsx
+++ components/TransactionList.tsx
@@ -178,9 +178,12 @@ const TransactionList: React.FC<TransactionListProps> = ({ transactions, currenc
     switch (category) {
       case 'Food & Dining':
         return (
-          // Knife icon
+          // Crossed fork and knife
           <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M12 4v16m0 0l-3-3m3 3l3-3m0-13l3 3M9 7l-3 3" />
+            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m16 2-2.3 2.3a3 3 0 0 0 0 4.2l1.8 1.8a3 3 0 0 0 4.2 0L22 8" />
+            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M15 15 3.3 3.3a4.2 4.2 0 0 0 0 6l7.3 7.3c.7.7 2 .7 2.8 0L15 15Zm0 0 7 7" />
+            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m2.1 21.8 6.4-6.3" />
+            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m19 5-7 7" />
           </svg>
         );
       case 'Shopping':
